fae5a95f85cf6c641727bfe1d68dec473cc8728f
Vivek Jha <vivek.x.jha@gmail.com>
2017-11-15 15:52:55 -0800

    Added dimensionsEqual to utility file

--- utility.py
+++ utility.py
@@ -12,3 +12,17 @@ def abspaths_data(data_dir):
 	abspaths_data_files = (os.path.join(abspath_data_dir, data) for data in os.listdir(abspath_data_dir))
 
 	return abspaths_data_files
+
+
+def dimensionsEqual(rows_list, columns_list, dimensions_list):
+	"""
+	:param rows_list: (sequence-type of ints) elements represent # of rows for a collection of dataframes
+	:param columns_list: (sequence-type of ints) elements represent # of rows for a collection of dataframes
+	:param dimensions_list: (sequence-type of ordered pairs) elements represent dimensions/shape for a collection of dataframes
+	:return: (bool) Used to assert that given rows_list/columns_list have same shape as dimensions_list
+	"""
+	d_list = zip(rows_list, columns_list)
+	for dim1, dim2 in zip(d_list, dimensions_list):
+		if dim1 != dim2:
+			return False
+	return True
