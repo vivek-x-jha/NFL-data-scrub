c5cdba4d2b541905946fbdca7f74baa11f596bbd
Vivek Jha <vivek.x.jha@gmail.com>
2017-11-15 03:06:31 -0800

    Consolidated comments for play-by-play dataframe dictionary into single line

--- datadiscovery.py
+++ datadiscovery.py
@@ -8,9 +8,7 @@ period = range(2009, 2018)
 # Creates generator expression containing absolute paths of play-by-play .csv files
 abspaths_data_files = abspaths_data('nflscrapR-data/season_play_by_play/')
 
-# Load data as Dictionary
-## keys -> season
-## values -> play-by-play Dataframes
+# Load data as Dictionary: values are play-by-play Dataframes (keyed by season)
 pbp_dataframes = {season: pd.read_csv(file) for season, file in zip(period, abspaths_data_files)}
 
 # Checks if pbp_dataframes have same number of columns
